2196fd7fd16b76df22146984bfab463ca88214d9
priveron <paul.riveron@outlook.com>
2021-09-29 10:48:04 +0200

    [ADD] init

---
+++ app.js
@@ -0,0 +1,5 @@
+
+const number = document.querySelector("#number");
+const color = document.querySelector("#color");
+const create = document.querySelector("#ceate");
+const boxContainer = document.querySelector("#box-container");
