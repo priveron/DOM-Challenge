c04e6dc56f78edd4a7142f3f05dc0ca0b700fb41
priveron <paul.riveron@outlook.com>
2021-09-29 11:16:58 +0200

    [ADD] simple create

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 
 const number = document.querySelector("#number");
 const color = document.querySelector("#color");
-const create = document.querySelector("#ceate");
+const create = document.querySelector("#create");
+const remove = document.querySelector("#remove-selected");
+const reset = document.querySelector("#reset");
 const boxContainer = document.querySelector("#box-container");
+
+create.addEventListener("click", () => {
+    for (let i = 0; i < number.value; i++) {
+        const el = document.createElement("div");
+        el.style.backgroundColor = color.value;
+        boxContainer.appendChild(el);
+    }
+});
+
+remove.addEventListener("click", () => {
+    
+});
+
+reset.addEventListener("click", () => {
+    
+});
