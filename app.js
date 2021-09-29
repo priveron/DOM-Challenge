e27039fdf8935f766211e57d48e5c3ce7219631d
priveron <paul.riveron@outlook.com>
2021-09-29 11:52:09 +0200

    [ADD] logic v1.0

--- app.js
+++ app.js
@@ -7,24 +7,30 @@ const reset = document.querySelector("#reset");
 const boxContainer = document.querySelector("#box-container");
 
 function updateNumbers() {
-    for (let i = 0; i < boxContainer.childNodes.length; i++) {
-        boxContainer.childNodes[i].textContent = i + 1;
-    }
+//     for (let i = 0; i < boxContainer.childNodes.length; i++)
+//         boxContainer.childNodes[i].textContent = i + 1;
+        boxContainer.childNodes.forEach((v, i) => v.textContent = i + 1);
 }
 
 create.addEventListener("click", () => {
     for (let i = 0; i < number.value; i++) {
         const el = document.createElement("div");
         el.style.backgroundColor = color.value;
+        el.addEventListener("click", () => {
+            el.classList.toggle("is-active");
+        });
         boxContainer.appendChild(el);
     }
     updateNumbers();
 });
 
 remove.addEventListener("click", () => {
-    
+    let box = [...boxContainer.childNodes].filter((v) => v.classList.contains("is-active"));
+    box.forEach((v) => boxContainer.removeChild(v));
+    updateNumbers();
 });
 
 reset.addEventListener("click", () => {
-    
+    [...boxContainer.childNodes].forEach((v) => boxContainer.removeChild(v));
+    updateNumbers();
 });
