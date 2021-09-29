390cf474d1493f47a0ffcc8ddb08b2f250214259
priveron <paul.riveron@outlook.com>
2021-09-29 11:22:24 +0200

    [FIX] looking good create

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const remove = document.querySelector("#remove-selected");
 const reset = document.querySelector("#reset");
 const boxContainer = document.querySelector("#box-container");
 
+function updateNumbers() {
+    for (let i = 0; i < boxContainer.childNodes.length; i++) {
+        boxContainer.childNodes[i].textContent = i + 1;
+    }
+}
+
 create.addEventListener("click", () => {
     for (let i = 0; i < number.value; i++) {
         const el = document.createElement("div");
         el.style.backgroundColor = color.value;
         boxContainer.appendChild(el);
     }
+    updateNumbers();
 });
 
 remove.addEventListener("click", () => {
