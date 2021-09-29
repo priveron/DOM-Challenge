3faa9397a64fe72c00fccb131f7736ca15915536
Pauline Bertrand <bertrand.pauline@gmail.com>
2021-09-29 12:20:00 +0200

    change text color depending on background

--- app.js
+++ app.js
@@ -12,10 +12,22 @@ function updateNumbers() {
         boxContainer.childNodes.forEach((v, i) => v.textContent = i + 1);
 }
 
+function determineTextColor(hexcolor) {
+    // this is copied and pasted from stackoverflow 
+    // https://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area
+    hexcolor = hexcolor.replace("#", "");
+    var r = parseInt(hexcolor.substr(0,2),16);
+    var g = parseInt(hexcolor.substr(2,2),16);
+    var b = parseInt(hexcolor.substr(4,2),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? 'black' : 'white';
+}
+
 create.addEventListener("click", () => {
     for (let i = 0; i < number.value; i++) {
         const el = document.createElement("div");
         el.style.backgroundColor = color.value;
+        el.style.color = determineTextColor(color.value);
         el.addEventListener("click", () => {
             el.classList.toggle("is-active");
         });
